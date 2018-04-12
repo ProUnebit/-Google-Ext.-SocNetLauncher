1f693c19f421aef6467b7a87634fedcd8b68662c
ProUnebit <ProUnebit@yandex.ru>
2018-04-12 17:21:20 +0300

    add bootstrap, html is change, add new "ogle files"

--- popup.js
+++ popup.js
@@ -1,7 +1,1 @@
-const title = document.querySelector('.modal-header__main-title');
-const btn = document.querySelector('.modal-header__button');
 
-btn.addEventListener('click', () => {
-    title.style.color = "violet";
-    title.innerHTML = "The title is changed!";
-});
