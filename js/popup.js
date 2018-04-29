5c896e5d2526507a8b7ff7d36f9f15402cbe7ed0
ProUnebit <ProUnebit@yandex.ru>
2018-04-29 05:15:34 +0300

    try fix "Unauthorized"

--- js/popup.js
+++ js/popup.js
@@ -1,21 +1,21 @@
-const secondHand = document.querySelector('.second-hand');
-const minsHand = document.querySelector('.min-hand');
-const hourHand = document.querySelector('.hour-hand');
+const s_Hand = document.querySelector('.s-hand');
+const m_Hand = document.querySelector('.m-hand');
+const h_Hand = document.querySelector('.h-hand');
 
-function setDate() {
+function viewDate() {
   const now = new Date();
 
   const seconds = now.getSeconds();
   const secondsDegrees = ((seconds / 60) * 360) + 90;
-  secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+  s_Hand.style.transform = `rotate(${secondsDegrees}deg)`;
 
   const mins = now.getMinutes();
   const minsDegrees = ((mins / 60) * 360) + ((seconds/60)*6) + 90;
-  minsHand.style.transform = `rotate(${minsDegrees}deg)`;
+  m_Hand.style.transform = `rotate(${minsDegrees}deg)`;
 
   const hour = now.getHours();
   const hourDegrees = ((hour / 12) * 360) + ((mins/60)*30) + 90;
-  hourHand.style.transform = `rotate(${hourDegrees}deg)`;
+  h_Hand.style.transform = `rotate(${hourDegrees}deg)`;
 }
 
-setInterval(setDate, 1000);
+setInterval(viewDate, 1000);
